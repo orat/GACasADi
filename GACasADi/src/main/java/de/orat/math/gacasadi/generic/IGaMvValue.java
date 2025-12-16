6851fb139099f2de05eef37a5d0bf07488418efa
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-12-16 13:42:04 +0100

    Added MvValue::get

--- GACasADi/src/main/java/de/orat/math/gacasadi/generic/IGaMvValue.java
+++ GACasADi/src/main/java/de/orat/math/gacasadi/generic/IGaMvValue.java
@@ -2,9 +2,27 @@ package de.orat.math.gacasadi.generic;
 
 import de.dhbw.rahmlab.casadi.impl.casadi.DM;
 import de.orat.math.gacalc.spi.IMultivectorValue;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface IGaMvValue<VAL extends IGaMvValue<VAL, EXPR>, EXPR extends IGaMvExpr<EXPR>>
     extends IMultivectorValue<VAL, EXPR>, IGetSparsityCasadi {
 
     DM getDM();
+
+    default double get(int index) {
+        DM dm = getDM();
+        double value = dm.at(index, 0).scalar();
+        return value;
+    }
+
+    default List<Double> get(List<Integer> indices) {
+        DM dm = getDM();
+        List<Double> values = new ArrayList<>(indices.size());
+        for (int index : indices) {
+            double value = dm.at(index, 0).scalar();
+            values.add(value);
+        }
+        return values;
+    }
 }
